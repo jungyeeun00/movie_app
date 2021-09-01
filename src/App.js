48612ab225b811faaaa10c33407ec4aca9ed9ce1
jungyeeun00 <ekfqlc0103@naver.com>
2021-09-01 20:37:03 +0900

    chapter 3

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id:1,
-    name:"kimbap",
-    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dgBdvLWjQph6xLPpF6_u3AHaE8%26pid%3DApi&f=1",
-    rating: 4.3
-  },
-  {
-    id:2,
-    name:"kimchi",
-    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Iy6uvQLmhDuJnGSUmXDT8AHaEK%26pid%3DApi&f=1",
-    rating: 5.0
-  },
-  {
-    id:3,
-    name:"mango",
-    image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.h9wHdTpLCRRiyHIzvMNaFAHaFZ%26pid%3DApi&f=1",
-    rating: 2.5
+class App extends React.Component{
+  state =   {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading:false})
+    }, 6000);
   }
-]
-
-function Food({name, picture, rating}){
-  return(
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture}/>
-    </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return (
-    <div className="App">
-        {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)}
-    </div>
-  );
+  render(){
+    const {isLoading} = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
 }
 
 export default App;
